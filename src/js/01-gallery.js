b6e1b01eadb70e6bd862500febbc89bb2f96c5f0
Oksana <oksana.sydorenko0691@icloud.com>
2023-02-27 20:21:14 +0200

    hw8

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+function makeGalleryMarkup(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
+console.log(galleryItems);
