45eda74e0a1923a5d6820bb00b95d1dcfa55565a
Oksana <oksana.sydorenko0691@icloud.com>
2023-03-01 21:04:37 +0200

    hw8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,16 +5,16 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const STORAGE_TIME_KEY = 'videoplayer-current-time';
+const STORAGE_TIME_KEY = 'videoplayer-current-time'; //змінна з ключем збереження та отримання поточного часу відео в об"єкті 
 
 const currentTime = localStorage.getItem(STORAGE_TIME_KEY)
   ? localStorage.getItem(STORAGE_TIME_KEY)
-  : 0;
+  : 0; //присвоюється значення, отримане з ключа
 
 function getCurrentTime(e) {
-  localStorage.setItem(STORAGE_TIME_KEY, e.seconds);
+  localStorage.setItem(STORAGE_TIME_KEY, e.seconds); //функція встановлює поточний час відео 
 }
 
-player.on('timeupdate', throttle(getCurrentTime, 1000));
+player.on('timeupdate', throttle(getCurrentTime, 1000)); //Функція використовується для обмеження частоти викликів(раз в секунду)
 
-player.setCurrentTime(currentTime);
+player.setCurrentTime(currentTime); //метод викликається для Player об’єкта зі currentTime значенням, яке встановлює поточний час відео на значення, отримане з localStorage
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,8 +10,8 @@ refs.form.addEventListener('input', throttle(onInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
 const STORAGE_INPUT_KEY = 'feedback-form-state';
-
-function onInput(e) {
+//Коли користувач вводить інформацію у форму, функція викликається із затримкою 500 мс за допомогою бібліотеки 
+function onInput(e) { // функція зберігає дані, введені користувачем, у локальному сховищі під ключем 
   const userDetails = JSON.parse(localStorage.getItem(STORAGE_INPUT_KEY)) || {};
   console.log(userDetails);
   userDetails[e.target.name] = e.target.value;
@@ -22,7 +22,7 @@ function onInput(e) {
 const saveLocalItems = localStorage.getItem(STORAGE_INPUT_KEY);
 const parsSaveLocalItems = JSON.parse(saveLocalItems);
 
-function getLocalStorageItems() {
+function getLocalStorageItems() { //функція перевіряє, чи є збережені дані в локальному сховищі, і якщо так, то встановлює значення полів введення на збережені значення.
   if (saveLocalItems) {
     !parsSaveLocalItems.email
       ? ''
@@ -35,7 +35,7 @@ function getLocalStorageItems() {
 
 getLocalStorageItems();
 
-function onFormSubmit(e) {
+function onFormSubmit(e) {//перевіряє чи внесені поля, якщо ні виводить повідомлення
   e.preventDefault();
 
   const {
@@ -43,7 +43,7 @@ function onFormSubmit(e) {
   } = e.currentTarget;
 
   if (email.value === '' || message.value === '') {
-    return alert('Введіть всі поля форми!!!');
+    return alert('Введіть всі поля форми!');
   }
 
   const formElDetails = { email: email.value, message: message.value };
@@ -51,8 +51,10 @@ function onFormSubmit(e) {
 
   e.currentTarget.reset();
 
-  removeStorageItems();
+  removeStorageItems(); 
+
+
 }
-function removeStorageItems() {
+function removeStorageItems() { // функція, яка видаляє збережені дані з локального сховища.
   localStorage.removeItem(STORAGE_INPUT_KEY);
 }
